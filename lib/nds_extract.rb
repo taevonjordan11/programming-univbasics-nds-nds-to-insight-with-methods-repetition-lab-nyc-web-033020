0137b4283b01310d631325d59d1970ef649bf1de
Taevon Jordan <taevonjordan1@gmail.com>
2020-03-12 20:44:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,7 +42,16 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  
+   dir_totals= directors_totals(source)
+  name_to_check = list_of_directors(source)
+  name_count = 0 
+  sum = 0 
+  while name_to_check[name_count] do
+    sum += dir_totals[name_to_check[name_count]]
+    name_count += 1 
+  end
+  sum
+end
 end
 
 
